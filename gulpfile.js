2c8b9300199a80a619aeb28f41488a47fe9de24b
Alex Bedley <aeoliant@hotmail.com>
2015-03-20 20:04:51 -0400

    set env variable

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,24 @@
 
 var gulp = require('gulp'),
 	versionz = require('./index'),
-	jshint = require('gulp-jshint');
+	jshint = require('gulp-jshint'),
+	exec = require('child_process').exec;
 
-gulp.task('publish', function() {
+gulp.task('publish', function(cb) {
 	versionz(function(err, info) {
 		if (err) {
-			console.log(err);
+			cb(err);
 		} else if (info.changed) {
 			console.log('package.json version changed from ' + info.then + ' to ' + info.now + ' last commit');
-			process.env.RELEASE_VERSIONZ = true;
+			exec('export RELEASE_VERSIONZ=true', function(error, stdout, stderr) {
+				if (error) {
+					return cb(error);
+				}
+				cb();
+			});
 		} else {
 			console.log('package.json did not change since last commit');
+			cb();
 		}
 	});
 });
