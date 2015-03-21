cea9d7b5001c8d7382308170708100e796321974
Alex Bedley <aeoliant@hotmail.com>
2015-03-20 23:10:37 -0400

    add quiet flag to cli

--- bin/versionz.js
+++ bin/versionz.js
@@ -3,14 +3,30 @@
 var cli = require('cli'),
 	versionz = require('../index.js');
 
-cli.main(function() {
+cli.parse({
+	quiet: ['q', 'quiet mode']
+});
+
+cli.main(function(args, options) {
 	versionz(function(err, info) {
 		if (err) {
-			cli.fatal(err);
+			if (options.quiet) {
+				cli.exit(1);
+			} else {
+				cli.fatal(err);
+			}
 		} else if (!info.changed) {
-			cli.fatal('package.json did not change since last commit');
+			if (options.quiet) {
+				cli.exit(1);
+			} else {
+				cli.fatal('package.json did not change since last commit');
+			}
 		} else {
-			cli.ok('package.json changed from ' + info.then + ' to ' + info.now);
+			if (options.quiet) {
+				cli.exit();
+			} else {
+				cli.ok('package.json changed from ' + info.then + ' to ' + info.now);
+			}
 		}
 	});
 });
